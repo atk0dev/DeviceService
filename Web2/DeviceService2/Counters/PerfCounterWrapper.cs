8c74fd233f0e5292962d12782f0da6e595647658
Andrii <at@winkas.dk>
2017-03-31 12:43:48 +0300

    Added Performance counter chart

---
+++ Web2/DeviceService2/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace DeviceService2.Counters
+{
+    public class PerfCounterWrapper
+    {
+        public PerfCounterWrapper(string name, string category, string counter, string instance = "")
+        {
+            this._counter = this.CreatePerformanceCounter(name, category, counter, instance);
+            Name = name;
+        }
+
+        
+
+        public string Name { get; set; }
+
+        public float Value
+        {
+            get { return this.GetNextValue(); }
+        }
+
+        private PerformanceCounter _counter;
+
+        private PerformanceCounter CreatePerformanceCounter(string name, string category, string counter, string instance = "")
+        {
+            try
+            {
+                return new PerformanceCounter(category, counter, instance, readOnly: true);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        private float GetNextValue()
+        {
+            if (this._counter == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return this._counter.NextValue();
+            }
+            
+        }
+    }
+}
--- Web2/DeviceService2/Hubs/PerfHub.cs
+++ Web2/DeviceService2/Hubs/PerfHub.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using DeviceService2.Counters;
 using Microsoft.AspNet.SignalR;
 
 namespace DeviceService2.Hubs
 {
     public class PerfHub : Hub
     {
+        public PerfHub()
+        {
+            StartCounterCollection();
+        }
+
+        private void StartCounterCollection()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                var perfService = new PerfCounterService();
+                while (true)
+                {
+                    var results = perfService.GetResults();
+                    Clients.All.newCounters(results);
+                    await Task.Delay(2000);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
         public void Send(string message)
         {
             string userName = string.Empty;
